5e38f4aad88bd2da2c16874ea2593e0cf38b980c
ArthurH0 <arthur.acacio@hotmail.com>
2024-10-10 18:02:49 -0300

    Reestruturação do projeto e Criação do back-end

---
+++ src/BibliotecaAnuncarBack/Server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(3000);
+
+console.log('Servidor Rodando na porta 3000')
