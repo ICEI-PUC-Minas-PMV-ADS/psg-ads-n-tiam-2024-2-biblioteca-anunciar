b606e1e643d6a6020cbfc4177f1f89459ddc319b
ArthurH0 <arthur.acacio@hotmail.com>
2024-10-10 18:48:42 -0300

    Add FrontEnd

--- src/Biblioteca Anunciar Code/BibliotecaAnuncarBack/Server.js
+++ src/Biblioteca Anunciar Code/BibliotecaAnuncarBack/Server.js
@@ -3,8 +3,8 @@ const app = express()
 
 app.get('/', (req, res) => {
   res.send('hello world')
-})
+});
 
 app.listen(3000);
 
-console.log('Servidor Rodando na porta 3000')
+console.log('Servidor Rodando na porta 3000');
---
+++ src/Biblioteca Anunciar Code/BibliotecaAnunciarFront/components/navbar/navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+
+export default function Navbar() {
+    return (
+        <View style={styles.navbar}>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    navbar: {
+        width: '100%', 
+        height: 140, 
+        position: 'absolute', 
+        top: 0, 
+        justifyContent: 'center',
+        alignItems: 'center', 
+        backgroundColor: '#fff', 
+        elevation: 4, 
+        resizeMode: 'contain', 
+    },
+    image: {
+        width: '100%', 
+        /* height: '100%', */
+        resizeMode: 'contain', 
+    },
+});
